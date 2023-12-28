5a2287a3cc42531f615c533a625f9d9a6e6487f1
Developer-Ayan <58031124+developer-ayan@users.noreply.github.com>
2023-12-28 14:29:33 +0500

    Update conn.js

--- src/config/conn.js
+++ src/config/conn.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
-const { BASE_URL, DATA_BASE } = require('../utils/urls');
 
-console.log('testing url',  process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
 
-mongoose.connect(process.env.MONGO_URL)
-// mongoose.connect(`${BASE_URL}/${DATA_BASE}`)
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((error) => {
-        console.error("Error connecting to MongoDB:", error.message);
-    });
