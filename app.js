8d3f50f8fe4a5ebecbcbbe1bbfdaa639311712f5
Developer-Ayan <58031124+developer-ayan@users.noreply.github.com>
2024-01-01 14:45:50 +0500

    * file access done

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ require('dotenv').config();
 require('./src/config/conn');
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use('/uploads/station_images', express.static('uploads/station_images'));
+app.use('/uploads/users', express.static('uploads/users'));
 
 const authRoutes = require('./src/api/routers/auth-routers');
 const loggedInRoutes = require('./src/api/routers/logged-in-routers');
 const adminRoutes = require('./src/api/routers/admin-routers');
+
 
 app.use('/auth', authRoutes);
 app.use('/app/logged_in', loggedInRoutes);
