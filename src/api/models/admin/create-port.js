4387c0ade80a345713aa50e756f35aae08650304
developerLawsyst <>
2024-06-05 16:05:35 +0500

    * create station port add new fields

--- src/api/models/admin/create-port.js
+++ src/api/models/admin/create-port.js
@@ -1,44 +1,54 @@
-const mongoose = require('mongoose');
-const { created_at } = require('../../../utils/static-values');
+const mongoose = require("mongoose");
+const { created_at } = require("../../../utils/static-values");
 
 const portSchema = new mongoose.Schema({
-    station_id: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    port_type: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    unit_price: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    port_name: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    port_description: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    port_image: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    date: {
-        type: String,
-        trim: true,
-        default: created_at,
-    },
+  station_id: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  port_type: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  unit_price: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  port_name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  port_description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  charger_id: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  connector_id: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  port_image: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  date: {
+    type: String,
+    trim: true,
+    default: created_at,
+  },
 });
 
-const Ports = mongoose.model('ports', portSchema);
+const Ports = mongoose.model("ports", portSchema);
 
 module.exports = Ports;
