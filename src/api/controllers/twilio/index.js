ab7053e9dc19c19103fcd9e020e169d9fcdaf440
developerLawsyst <>
2024-09-12 20:54:40 +0500

    * bug fix login

--- src/api/controllers/twilio/index.js
+++ src/api/controllers/twilio/index.js
@@ -1,15 +1,19 @@
-const accountSid = 'ACfc3ba06ba47f608d251bce7b6693a35b';
-const authToken = '387448c675cdeea1c0b4bf8d07b3f3bb';
-
-const SendSms = (phone, message) => {
-  const client = require('twilio')(accountSid, authToken);
-  client.messages
-    .create({
-       body: message,
-       from: '+17198736018',
-       to: phone
-     })
-    .then(message => console.log(message.sid));
-}
+const SendSms = async (phone, message) => {
+  const accountSid = "ACfc3ba06ba47f608d251bce7b6693a35b";
+  const authToken = "387448c675cdeea1c0b4bf8d07b3f3bb"; 
+  const client = require("twilio")(accountSid, authToken);
+  try {
+    const sentMessage = await client.messages.create({
+      body: message,
+      // from: "EVCharging SMS", // Replace with your Twilio phone number
+      to: phone,
+    });
+    console.log(`Message SID: ${sentMessage.sid}`);
+    return otp; // Return OTP
+  } catch (error) {
+    console.error(`Error sending SMS: ${error}`);
+    throw error; // Propagate the error
+  }
+};
 
 module.exports = SendSms;
